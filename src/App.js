b2b5c04dcdfc2f103b25fe469036f551bfb06f97
Deekshiba <deekshibapv@gmail.com>
2025-04-14 00:22:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import './App.css';
 import React from "react";
-import { Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
     <div className="App">
-    <header className="App-header">
-    <Router>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
-    </Router>
+      <header className="App-header">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </Router>
       </header>
     </div>
   );
 };
 
 export default App;
-
-
